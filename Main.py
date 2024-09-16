78d51452d2051b5af6e6cee7d1af73daf67621d0
ofirraz12 <ofir.work.raz@gmail.com>
2024-09-16 11:16:49 +0300

    state + handle user events

--- Main.py
+++ Main.py
@@ -1,5 +1,20 @@
-print("yello")
-print("!!")
-print("round 2")
-print("last try")
-print("sssss")
+import pygame
+
+state = {
+    "state": consts.RUNNING_STATE,
+    "is movement": False,
+    "is_window_open": True,
+}
+
+
+def handle_user_events():
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            state["is_window_open"] = False
+
+        elif state["state"] != consts.RUNNING_STATE:
+            pass
+
+        if event.key == pygame.K_SPACE:
+            pass
