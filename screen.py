f943fa1e7e7cbaa71bb8ada0fd38b83550b23ac3
try <orgelb06@gmail.com>
2024-09-16 12:13:47 +0300

    screen

--- screen.py
+++ screen.py
@@ -1,8 +1,23 @@
 import pygame
 import consts
 
-def create_screen(width,height,window_color):
 
+def create_screen(width, height, window_color):
     pygame.init()
     surface = pygame.display.set_mode((width, height))
     surface.fill(window_color)
+    pygame.display.flip()
+
+
+create_screen(consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT, consts.WINDOW_COLOR)
+running = True
+
+# game loop
+while running:
+
+    # for loop through the event queue
+    for event in pygame.event.get():
+
+        # Check for QUIT event
+        if event.type == pygame.QUIT:
+            running = False
